828cc32bd8163d188f2e5d7b0c5fbdf2a5957ad9
Ishan Kumar <ishanakshar@gmail.com>
2023-08-18 17:39:48 +0530

    feat: Chat-App working

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,118 +1,118 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import {GetServerSideProps, InferGetServerSidePropsType, InferGetStaticPropsType, NextPage} from "next"
+import { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps = async(context) => {
+  const response = await fetch(process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string, 
+    {
+      method: 'POST',
+      headers:{
+        'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET as string
+      },
+      body: JSON.stringify({
+        query: `query {
+          messages {
+            id
+            message
+            created_at
+          }
+        }`
+      })
+  })
+  const result = await response.json();
+
+  return {
+    props: { messages: await result.data.messages }
+  }
+}
+const handleDelete = async (id: any) => {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string, {
+      method: 'POST',
+      headers: {
+        'x-hasura-admin-secret': "ADh9Ip7eUqMlsbTEsOo1ZHeEksmR2cYI8Uqqak0IWYrtKsKv1bG58xtNgNaUbo1x", 
+      },
+      body: JSON.stringify({
+        query: `
+          mutation DeleteMessage($id: Int!) {
+            delete_messages(where: { id: { _eq: $id } }) {
+              affected_rows
+            }
+          }
+        `,
+        variables: {
+          id: id,
+        },
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (responseData.data) {
+      window.location.reload();
+    } else {
+      console.error('Deleting message failed:', responseData.errors);
+    }
+  } catch (error) {
+    console.error('Error deleting message:', error);
+  }
+};
+
+export default function Home({ messages}:any) {
+  const [message, setMsg] = useState("");
+  const handleSubmit= async(e:any)=>{
+    e.preventDefault();
+    try{
+      const res = await fetch(process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string,
+        {
+          method: 'POST',
+          headers: {
+            'x-hasura-admin-secret': "ADh9Ip7eUqMlsbTEsOo1ZHeEksmR2cYI8Uqqak0IWYrtKsKv1bG58xtNgNaUbo1x",
+          },
+          body: JSON.stringify({
+            query: `mutation InsertMessages($message: String) {
+              insert_messages(objects: {message: $message}) {
+                affected_rows
+              }
+            }`,
+            variables: {
+              message: message
+            }
+          })
+        })
+        setMsg("");/* This is to prevent memory leaks */
+        window.location.reload();
+    }catch(error){
+      console.log(error);
+      window.alert('Something went wrong :(')
+    }
+      
+  }
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 bg-gray-700`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='w-[500px] h-[400px] bg-white border-black border-2 p-2 flex flex-col items-center justify-between'>
+       <div className='w-[300px] h-[400px] bg-white border-black flex flex-col items-center justify-between'>
+      <div className='flex flex-col items-end w-[500px] p-1 gap-2'>
+      {messages.map((message:any) => (
+        <div key={message.id} className='flex flex-row gap-2 p-2 border border-black rounded-xl rounded-br-none'>
+          <button onClick={()=>handleDelete(message.id)} className='text-black text-[10px]'>Delete</button>
+        <p className='text-black text-md max-w-[200px]'>{message.message}</p>
+        
         </div>
+      ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-[490px] flex flex-row items-center border'>
+        {/** input and a button */}
+        <input className='flex flex-1 h-[40px] outline-none p-1 text-black' onChange={(e)=>setMsg(e.target.value)} value={message} placeholder='Say something....'/>
+        <button onClick={(e)=>handleSubmit(e)} className='h-[40px] text-black border p-1 hover:bg-red-100 duration-300'>Send</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
+      </div>
+      
     </main>
   )
 }
